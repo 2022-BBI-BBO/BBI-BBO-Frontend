f1c2b51287a5e98c0edbc413294b48e791b95f2b
Hahmjimin <202110315@bssm.hs.kr>
2022-09-29 19:19:30 +0900

    error

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -9,19 +9,21 @@ class Photo extends Component {
       <div>
         <Header />
         <form>
-          <button className="left">⬅️</button>
-          <button className="right">➡️</button>
-          <div className="num">
-            <button className="num1">1</button>&emsp;&emsp;&emsp;
-            <button className="num2">2</button>&emsp;&emsp;&emsp;
-            <button className="num3">3</button>&emsp;&emsp;&emsp;
-            <button className="num4">4</button>&emsp;&emsp;&emsp;
-            <button className="num5">5</button>&emsp;&emsp;&emsp;
-            <button className="num6">6</button>&emsp;&emsp;&emsp;
-            <button className="num7">7</button>&emsp;&emsp;&emsp;
-            <button className="num8">8</button>&emsp;&emsp;&emsp;
-            <button className="num9">9</button>&emsp;&emsp;&emsp;
-            <button className="num10">10</button>
+          <div className="page">
+            <ul className="pagenation">
+              <a href="#" class="first">이전</a>&nbsp;&nbsp;
+              <a href="#" class="num">1</a>&nbsp;
+              <a href="#" class="num">2</a>&nbsp;
+              <a href="#" class="num">3</a>&nbsp;
+              <a href="#" class="num">4</a>&nbsp;
+              <a href="#" class="num">5</a>&nbsp;
+              <a href="#" class="num">6</a>&nbsp;
+              <a href="#" class="num">7</a>&nbsp;
+              <a href="#" class="num">8</a>&nbsp;
+              <a href="#" class="num">9</a>&nbsp;&nbsp;
+              <a href="#" class="last">다음</a>
+            </ul>
+
           </div>
           <div className="photo1"></div>
           <div className="photo2"></div>
@@ -33,7 +35,13 @@ class Photo extends Component {
           <div className="photo6"></div>
           <div className="photo7"></div>
           <div className="photo8"></div>
+
+          <div class="paging-div">
+            <ul class="pagination" id="pagination"></ul>
+          </div>
+
         </form>
+
       </div>
     );
   }
