91d428b9e87923e89fde531e905d5163ad865d95
Hahmjimin <80664856+Hahmjimin@users.noreply.github.com>
2022-11-20 21:38:57 +0900

    Revert "receive data"

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,30 +6,39 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Photo.css";
 import { Link } from "react-router-dom";
-const Photo = () => {
-    const [data,setData] = useState([]);
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(10);
-    const url = "http://10.150.150.2:8080/api/test";
-    const [dataId,setDataId] = useState([]);
-    const [dataPath,setDataPath] = useState([]);
-    const [dataDate,setDataDate] = useState([]);
-    useEffect( () => {axios.get(url).then(function(response) {setData(response.data); console.log("SUCCESS!!!");}).catch(function(error) {console.log("FAILD!!! ");});},[]);
-    useEffect( () => {
-        data.forEach( item => {
-            let id = String((item.substring(0,item.indexOf(','))));
-            setDataId(dataId=>[...dataId,',',id]);
-            let path = String( (item.substring( item.indexOf(',')+1,item.indexOf(',',item.indexOf(',')+1 ) ) ));
-            setDataPath(dataPath=>[...dataPath,',',path]);
-            let date = String(item.substring(item.lastIndexOf(',')));
-            setDataDate(dataDate=>[...dataDate,date]);
-        });
-        },[data]);
-    return (
+
+function Photo() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = (posts) => {
+    let currentPosts = 0;
+    currentPosts = posts.slice(indexOfFirst, indexOfLast);
+    return currentPosts;
+  };
+
+  console.log(posts);
+
+  return (
     <div>
       <Header />
+
       <div className="gallery">
         <table className="sub_news">
           <colgroup>
@@ -42,23 +51,17 @@ const Photo = () => {
               <th className="text-gray-300 px-4 py-3">Name</th>
             </tr>
           </thead>
-            {/*<Tr posts={currentPosts(posts)} loading={loading}></Tr>*/}
+          <Tr posts={currentPosts(posts)} loading={loading}></Tr>
         </table>
       </div>
-        <div>
-            I'm id { dataId }
-        </div>
-        <h2>
-            This is path { dataPath }
-        </h2>
-        <h3>date {dataDate}</h3>
 
-        {/*<Pagination
+      <Pagination
         postsPerPage={postsPerPage}
         totalPosts={posts.length}
         paginate={setCurrentPage}
-        ></Pagination>*/}
+      ></Pagination>
     </div>
   );
 }
+
 export default Photo;
